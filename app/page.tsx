55fb916531153ebc65cb59f901cc20c1ad86afac
Kalahe Liyanage <deeshanaliyanage@gmail.com>
2025-10-26 00:45:21 +0530

    Add testimonials component with styling and demo data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import {Card, CardFooter, Image, Button} from "@heroui/react";
 import {Aboutus} from "./component/AboutUs";
+import Testimonials from "./component/testimonials-component/testimonials";
 export default function Home() {
   return (
       <>
       <Aboutus />
+      <Testimonials/>
       </>
   );
 }
